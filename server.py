691976727168b5d4f215ba4fa5221cf0355df703
Flach Dániel <flachdaniel77@gmail.com>
2017-08-22 16:49:27 +0200

    Adding elements implemented

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, redirect, request, session
 import csv
+import uuid
+import os
+
 
 app = Flask(__name__)
+
+
+def generate_id():
+    new_id = uuid.uuid4()
+    return str(new_id)
 
 
 def read_csv():
@@ -14,9 +22,9 @@ def read_csv():
 
 
 def write_csv(stories):
-    with open("stories.csv") as csvfile:
+    with open("stories.csv","a") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(lst)
+        writer.writerow(stories)
 
 
 @app.route('/story')
@@ -34,10 +42,21 @@ def route_index():
 def route_edit_story(post_id):
     return render_template('story.html')
 
+@app.route('/save_story',methods=["POST"])
+def route_save_story():
+    list_label = ["storytitles","userstory","acceptance","business","estimation"]
+    stories = []
+    new_id = generate_id()
+    stories.insert(0, new_id)
+    for i in list_label:
+        stories.append(request.form[i])
+    write_csv(stories)
+    return redirect("/")
+
 
 if __name__ == "__main__":
-  app.secret_key = 'titkoskulcs'
-  app.run(
-      debug=True,  # Allow verbose error reports
-      port=5000  # Set custom port
-  )
+    app.secret_key = 'titkoskulcs'
+    app.run(
+        debug=True,  # Allow verbose error reports
+        port=5000  # Set custom port
+    )
