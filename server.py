5bb0cf1466c34e6e4b16a57379f1cbd065571e8e
Flach Dániel <flachdaniel77@gmail.com>
2017-08-23 12:30:12 +0200

    Delete now working, refactored

--- server.py
+++ server.py
@@ -21,10 +21,17 @@ def read_csv():
     return stories
 
 
-def write_csv(stories):
-    with open("stories.csv","a") as csvfile:
+def append_csv(row):
+    with open("stories.csv", "a") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(stories)
+        writer.writerow(row)
+
+
+def write_csv(stories):
+    with open("stories.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        for story in stories:
+            writer.writerow(story)
 
 
 @app.route('/story')
@@ -38,24 +45,46 @@ def route_index():
     return render_template('index.html', stories=stories)
 
 
-@app.route('/story/<int:post_id>')
-def route_edit_story(post_id):
+@app.route('/story')
+def route_new_story():
+    stories = read_csv()
     return render_template('story.html')
 
-@app.route('/save_story',methods=["POST"])
+
+@app.route('/save_story', methods=["POST"])
 def route_save_story():
-    list_label = ["storytitles","userstory","acceptance","business","estimation"]
-    stories = []
-    new_id = generate_id()
-    stories.insert(0, new_id)
+    list_label = ["storytitles", "userstory", "acceptance", "business", "estimation", "status"]
+    story = []
+    story.insert(0, generate_id())
     for i in list_label:
-        stories.append(request.form[i])
+        story.append(request.form[i])
+    append_csv(story)
+    return redirect("/")
+
+
+@app.route('/story/<post_id>')
+def route_edit_story(post_id):
+    stories = read_csv()
+    for row in stories:
+        if row[0] == post_id:
+            return render_template("story.html", row=row)
+
+
+@app.route('/delete/<post_id>')
+def route_delete_story(post_id):
+    stories = read_csv()
+    index = 0
+    for row in stories:
+        if row[0] == post_id:
+            popindex = index
+        index += 1
+    stories.pop(popindex)
     write_csv(stories)
     return redirect("/")
 
 
 if __name__ == "__main__":
-    app.secret_key = 'titkoskulcs'
+    app.secret_key = 'asdadd'
     app.run(
         debug=True,  # Allow verbose error reports
         port=5000  # Set custom port
